bfe9b4449bfe0a0d677f35b3740fdda667369ab1
yzhou <yzhou@amxc.tech>
2023-07-04 22:35:34 +0800

    1.修改版本号；

--- android/app/src/main/kotlin/com/zy/chatgpt/chatgpt/MainActivity.kt
+++ android/app/src/main/kotlin/com/zy/chatgpt/chatgpt/MainActivity.kt
@@ -7,6 +7,6 @@ import io.flutter.embedding.android.FlutterActivity
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        preInit(this, "649e6457a1a164591b3e7ed7", "huawei")
+        preInit(this, "649e6457a1a164591b3e7ed7", "xiaomi")
     }
 }
